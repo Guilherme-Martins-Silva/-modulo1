3ce804d05fa127eaba9b4f92386ec0489b2dd899
Guilherme-Martins-Silva <guilherme.silva@pdbomdespacho.com.br>
2024-09-03 09:51:18 -0300

    2.2 tipos de dados

---
+++ aula2_questao1_4.py
@@ -0,0 +1,50 @@
+"exercicio-1"
+print("Eercicio 1")
+n = 5
+m = 2
+print(type(n))
+print(n)
+print(type(m))
+print(m)
+o = n / m
+print(type(o))
+print(o)
+
+"exercicio-2"
+print("Exercicio 2")
+c=("E o valor e:")
+d=10
+e=3.5
+f=d+e
+print(c)
+print(f)
+print(type(c))
+print(type(f))
+
+
+"exercicio-3"
+print("Exercicio 3")
+v1=10
+v2=20
+v3=10
+v1=v1+v3
+v2=v1-v3
+print(v1)
+print(v2)
+print(v3)
+
+
+"exercicio-4"
+print("Exercicio 4")
+juros=1.01
+saldo=500
+print(saldo)
+saldo= saldo*juros
+print(saldo)
+saldo= saldo*juros
+print(saldo)
+saldo= saldo*juros
+print(saldo)
+
+
+
